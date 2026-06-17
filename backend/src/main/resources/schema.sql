2040f6ba9ae01912a631def4dd4367f590f93fa1
Karitchi <karichikarichikarichi@gmail.com>
2026-06-17 16:50:43 +0200

    replace admin seed with librarian user

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -50,7 +50,7 @@ INSERT INTO roles (name) VALUES ('admin'), ('librarian'), ('user');
 
 -- Insert default users (password: "testtest")
 INSERT INTO users (email, password, role_id) VALUES
-  ('admin@library.com', '$2a$10$l/InOIIhaW6C3.HENn5oPOf5NF.YUOLl0rLR76VKrHXoziCBLz7oq', 1),
+  ('librarian@library.com', '$2a$10$l/InOIIhaW6C3.HENn5oPOf5NF.YUOLl0rLR76VKrHXoziCBLz7oq', 2),
   ('test@test.com', '$2a$10$l/InOIIhaW6C3.HENn5oPOf5NF.YUOLl0rLR76VKrHXoziCBLz7oq', 3);
 
 -- Indexes
