3ee8408256515a5b5d1d1e4bd656f3b771c4180e
Karitchi <karichikarichikarichi@gmail.com>
2026-06-18 16:10:30 +0200

    underline librarian title

--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -5,7 +5,7 @@ export function Header() {
   return (
     <div className="flex items-center my-5">
       <div className="flex-1" />
-      <Link to="/books" className="text-center text-6xl font-jacquard">Librarian</Link>
+      <Link to="/books" className="text-center text-6xl font-jacquard underline">Librarian</Link>
       <div className="flex-1 flex justify-end">
         <AvatarMenu />
       </div>
