624670e6ddd4658df4e06d314834d1ab46f55b60
Karitchi <karichikarichikarichi@gmail.com>
2026-06-18 16:53:27 +0200

    hide avatar menu on signin/signup pages

--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -1,13 +1,16 @@
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 import { AvatarMenu } from "./AvatarMenu";
 
 export function Header() {
+  const { pathname } = useLocation();
+  const isSignPage = pathname === "/signin" || pathname === "/signup";
+
   return (
     <div className="flex items-center my-5">
       <div className="flex-1" />
       <Link to="/books" className="text-center text-6xl font-jacquard hover:underline">Librarian</Link>
       <div className="flex-1 flex justify-end">
-        <AvatarMenu />
+        {!isSignPage && <AvatarMenu />}
       </div>
     </div>
   );
